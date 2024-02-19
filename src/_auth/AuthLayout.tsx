95b85122700f578d9a078cbc19c8bc44920c688b
Walid Siala <wsiala4@gmail.com>
2024-02-19 11:22:50 +0100

    Fix Login UI layout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -9,13 +9,13 @@ const AuthLayout = () => {
         <Navigate to="/login" />
       ) : (
         <>
-          <section className="flex flex-1 justify-center items-center flex-col">
+          <section className="flex flex-1 justify-center items-center flex-col p-32 2xl:p-48">
             <Outlet />
           </section>
           <img
             src="/illustration.svg"
             alt="logo"
-            className="h-screen w-1/2 bg-no-repeat p-36"
+            className="h-screen w-1/2 bg-no-repeat p-36 2xl:p-48"
           />
         </>
       )}
--- src/_auth/forms/login.tsx
+++ src/_auth/forms/login.tsx
@@ -6,28 +6,33 @@ import { Button } from "@/components/ui/button";
 const Login = () => {
   return (
     <>
-      <div className="flex flex-col gap-5">
-        <img src="/logo.svg" alt="logo" className="w-24" />
-        <div>
-          <h1 className="font-extrabold text-5xl">Login to Your Account</h1>
-          <p className="mt-2">Welcome Back !</p>
+      <div className="w-full">
+        <div className="flex flex-col gap-5">
+          <img src="/logo.svg" alt="logo" className="w-24" />
+          <div>
+            <h1 className="font-extrabold text-5xl tracking-tighter">Login to Your Account</h1>
+            <p className="mt-2 font-normal tracking-tight text-base">Welcome Back !</p>
+          </div>
+          <div className="h-0.5 w-full bg-slate-100"></div>
         </div>
-        <div className="h-0.5 w-full bg-slate-100"></div>
-      </div>
-      <div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="email">Email</Label>
-          <Input type="email" id="email" placeholder="Email" />
+        <div className="flex flex-col gap-5 mt-5">
+          <div className="grid w-full items-center gap-1.5">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" id="email" placeholder="Email" />
+          </div>
+          <div className="grid w-full items-center gap-1.5">
+            <Label htmlFor="password">Password</Label>
+            <Input type="password" id="password" placeholder="Password" />
+          </div>
+          <div>
+          <div className="flex items-center space-x-2">
+            <Checkbox id="terms" />
+            <Label htmlFor="terms">Accept terms and conditions</Label>
+          </div>
+          <p className="text-xs text-slate-400 mt-1 ml-6">You agree to our Terms of Service and Privacy Policy.</p>
+          </div>
+          <Button className="mt-5">Log In</Button>
         </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="password">Password</Label>
-          <Input type="password" id="password" placeholder="Password" />
-        </div>
-        <div className="flex items-center space-x-2">
-          <Checkbox id="terms" />
-          <Label htmlFor="terms">Accept terms and conditions</Label>
-        </div>
-        <Button>Log In</Button>
       </div>
     </>
   );
