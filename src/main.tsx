2fa98ca866946ee50e42f86ab204186f7d24f1a6
Walid Siala <wsiala4@gmail.com>
2024-02-24 18:54:21 +0100

    Adding react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { QueryProvider } from "./lib/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
+  <BrowserRouter>
+    <QueryProvider>
+      <App />
+    </QueryProvider>
+  </BrowserRouter>
+);
