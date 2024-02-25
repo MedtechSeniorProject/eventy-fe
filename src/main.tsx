b9cda448e7174deefac16e745f21238d5b711b1a
Walid Siala <wsiala4@gmail.com>
2024-02-25 12:24:19 +0100

    mini fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { QueryProvider } from "./lib/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <QueryProvider>
       <App />
-    </QueryProvider>
   </BrowserRouter>
 );
