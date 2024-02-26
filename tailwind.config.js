cc5d9d3649d88d58a7e11ece4435887b15b025e7
Walid Siala <wsiala4@gmail.com>
2024-02-26 20:15:19 +0100

    Merge to Orange Chart

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "#0A387E",
+          DEFAULT: "#F16E00",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
