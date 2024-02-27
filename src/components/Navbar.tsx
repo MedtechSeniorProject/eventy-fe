70685b836d25caebf72cea8a578842e541835876
Walid Siala <wsiala4@gmail.com>
2024-02-27 13:46:38 +0100

    Fix Image Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { BarChart3, CalendarDays, Settings, UserCircle, Users } from 'lucide-react';
 import { Label } from '@radix-ui/react-label';
 import { useState } from 'react';
+import { Button } from './ui/button';
 
 function Navbar(
     { role }: { role: string }
@@ -14,11 +15,15 @@ function Navbar(
 
     return (
         <nav className="w-3/12 shadow-[1px_0px_7px_rgba(0,0,0,0.25)]">
-            <div className='w-full h-1/5 flex justify-center align-center p-[22%]' >
-                <img src="/logo.png" alt="Eventy logo" className='center' style={{ objectFit: 'contain' }} />
+            <div className='w-full h-1/5 flex justify-center align-center' >
+                <img src="/logo.svg" alt="Eventy logo" className='w-32' style={{ objectFit: 'contain' }} />
+            </div>
+            <div className='flex items-center mb-3'>
+                <div className='h-0.5 w-10 bg-black'></div>
+                <p className='px-2 font-semibold'>Navigation</p>
+                <div className='h-0.5 w-full bg-black'></div>
             </div>
             <ul className='links'>
-
                 {role === 'SUPERADMIN' && (
                     <>
                     <li className={`cursor-pointer p-1 pl-5 ${active === "profile" ? "bg-primary text-white" : ""}`} onClick={() => { handleClick("profile") }}>
@@ -76,7 +81,6 @@ function Navbar(
                     </>
                 )}
             </ul>
-
         </nav>
     )
 }
