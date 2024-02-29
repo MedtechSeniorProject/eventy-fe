a19ee7de53ec1f1c08d17c4a7909fa9de820640c
Walid Siala <wsiala4@gmail.com>
2024-02-29 16:16:30 +0100

    Fix navbar

--- src/pages/eventmanager/NavLayoutEventManager.tsx
+++ src/pages/eventmanager/NavLayoutEventManager.tsx
@@ -7,13 +7,11 @@ const NavLayoutEventManager = () => {
 
     return (
         <div className="w-full">
-            <div className="w-full h-[7%] bg-black block box-border">
+            <div className="w-full fixed h-[7%] bg-black block box-border">
             </div>
-            <div className="flex w-full h-full gap-5">
+            <div className="flex">
                 <Navbar role="EVENTMANAGER" />
-                <div className="">
-                    <Outlet />
-                </div>
+                <Outlet />
             </div>
         </div>
     );
