0338ae3ceed07caf5c33ff7e0dcee75adad6dc30
Walid Siala <wsiala4@gmail.com>
2024-02-29 16:39:06 +0100

    Add Header

---
+++ src/pages/eventmanager/events/index.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react"
+import Header from "@/components/Header"
+const Events:FunctionComponent = () => { 
+
+  return (
+    <>
+        <div className="p-10">
+            <Header name="Dashboard" /> 
+        </div>
+    </>
+  )
+}
+
+export default Events
