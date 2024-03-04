e31117c173ef00b522865fe7462330b328bbb6bd
Walid Siala <wsiala4@gmail.com>
2024-03-04 13:25:41 +0100

    Changed password to 5 characters

--- src/lib/validations/auth.ts
+++ src/lib/validations/auth.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const loginSchema = z.object({
     email: z.string().email(),
-    password: z.string().min(7,"Password should contain at least 7 characters"),
+    password: z.string().min(5,"Password should contain at least 7 characters"),
     terms: z.boolean().default(false)
 }).required()
 
