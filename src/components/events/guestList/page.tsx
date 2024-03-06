e5a176936d4c8226447f05457f736fafdc19b509
Walid Siala <wsiala4@gmail.com>
2024-03-06 20:37:17 +0100

    Added fetch eventById

--- src/components/events/guestList/page.tsx
+++ src/components/events/guestList/page.tsx
@@ -1,87 +1,12 @@
 import { columns } from "./columns"
 import { DataTable } from "./data-table"
-import { Attendee } from "@/types/types";
-
-const data:Attendee[] = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@example.com",
-    addedBy: "Alice",
-    attended: true,
-  },
-  {
-    id: 2,
-    name: "Jane Smith",
-    email: "jane@example.com",
-    addedBy: "Bob",
-    attended: false,
-  },
-  {
-    id: 3,
-    name: "Michael Johnson",
-    email: "michael@example.com",
-    addedBy: "Alice",
-    attended: true,
-  },
-  {
-    id: 4,
-    name: "Emily Brown",
-    email: "emily@example.com",
-    addedBy: "Bob",
-    attended: false,
-  },
-  {
-    id: 5,
-    name: "David Wilson",
-    email: "david@example.com",
-    addedBy: "Alice",
-    attended: true,
-  },
-  {
-    id: 6,
-    name: "Sarah Taylor",
-    email: "sarah@example.com",
-    addedBy: "Bob",
-    attended: false,
-  },
-  {
-    id: 7,
-    name: "Christopher Lee",
-    email: "chris@example.com",
-    addedBy: "Alice",
-    attended: true,
-  },
-  {
-    id: 8,
-    name: "Olivia Moore",
-    email: "olivia@example.com",
-    addedBy: "Bob",
-    attended: false,
-  },
-  {
-    id: 9,
-    name: "Daniel White",
-    email: "daniel@example.com",
-    addedBy: "Alice",
-    attended: true,
-  },
-  {
-    id: 10,
-    name: "Emma Davis",
-    email: "emma@example.com",
-    addedBy: "Bob",
-    attended: false,
-  },
-];
 
 
-
-export default function GuestList() {
+export default function GuestList({...props}) {
 
   return (
     <div className="w-full">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={props.attendees} />
     </div>
   )
 }
--- src/pages/eventmanager/event/guestlist/index.tsx
+++ src/pages/eventmanager/event/guestlist/index.tsx
@@ -3,29 +3,48 @@ import EventHeader from "@/components/EventHeader"
 import GuestList from "@/components/events/guestList/page";
 import AddAttendee from "@/components/AddAttendee";
 import UploadAttendee from "@/components/UploadAttendee";
+import { useGetEventById } from "@/lib/queries/queries";
+import { useEffect, useState } from "react";
+import { EventAttendees } from "@/types/types";
 
 const EventGuestList = () => {
+  const {mutateAsync: getEventById, isLoading, isError } = useGetEventById()
+  const [event, setEvent] = useState<EventAttendees>({
+    id: "",
+    name: "",
+    time:"",
+    attendees: []
+  })
   let params = useParams();
 
-  // SEARCH EVENT BY ID
-  /** */
-  // DUMMY DATA
-  const data = {
-    name: "Spring Festival",
-    time: "Friday, April 5, 2024",
-    eventManager: "Amanda Johnson",
+  useEffect(() => {
+    const fetchEvent = async() => {
+      const response = await getEventById(params.id || "")
+      const responseData = await response.json()
+      console.log(responseData)
+      setEvent(responseData)
+    }
+    fetchEvent();
+  }, [params.id, getEventById])
+
+  if(isLoading){
+    return <div>Loading...</div>
+  }
+
+  if(isError){
+    return <div>An error has occurred...</div>
   }
 
   return (
     <div className="p-10 w-full">
-      <EventHeader name={data.name} time={data.time} />
+      <EventHeader name={event.name} time={event.time} />
       <div className="mt-5 flex gap-5">
         <AddAttendee />
         <UploadAttendee />
       </div>
-      <GuestList />
+      <GuestList attendees={event.attendees} />
     </div>
   )
 }
 
-export default EventGuestList
+export default EventGuestList;
