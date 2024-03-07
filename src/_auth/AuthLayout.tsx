8c1367062d993a9c3a56f1100302d3445d9b7019
Sarraj-Sahar <saharsarraj20.ss@gmail.com>
2024-03-07 21:15:48 +0100

    positioned auth page assets

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -7,14 +7,13 @@ const AuthLayout = () => {
   if (!isAuthenticated) {
     return (
       <>
-        <section className="flex flex-1 justify-center items-center flex-col p-32 2xl:p-48">
+        <section className="flex flex-1	 justify-center flex-col p-32 ">
+          <img className="absolute top-0 left-0 w-48 md:w-auto" src="/assets/topLeft.png" alt="image"/>
+          <img className="absolute top-0 right-0 w-48 md:w-auto" src="/assets/topRight.png" alt="image"/>
+          <img className="absolute bottom-0 right-0 w-48 md:w-auto" src="/assets/bottomRight.png" alt="image"/>
+          <img className="absolute bottom-0 left-0 w-48 md:w-auto" src="/assets/bottomLeft.png" alt="image"/>
           <Outlet />
         </section>
-        <img
-          src="/illustration.svg"
-          alt="logo"
-          className="h-screen w-1/2 bg-no-repeat p-36 2xl:p-48"
-        />
       </>
     );
   }
