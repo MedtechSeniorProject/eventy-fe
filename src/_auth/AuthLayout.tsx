b1877a82640fb21458623886296ae8e38a4a37b2
Sarraj-Sahar <saharsarraj20.ss@gmail.com>
2024-03-08 21:40:41 +0100

    updated login page ui

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -9,7 +9,8 @@ const AuthLayout = () => {
       <>
         <section className="flex flex-1	 justify-center flex-col p-32 ">
           <img className="absolute top-0 left-0 w-20 md:w-28 lg:w-40" src="/assets/topLeft.png" alt="image"/>
-          <img className="absolute top-0 right-0 w-20 md:w-28 lg:w-40" src="/assets/topRight.png" alt="image"/>
+          {/* //TODO: Add padding to top right image */}
+          <img className="absolute top-0 right-0 w-20 md:w-28 lg:w-36" src="/assets/topRight.png" alt="image"/>
           <img className="absolute bottom-0 right-0 w-20 md:w-28 lg:w-32" src="/assets/bottomRight.png" alt="image"/>
           <img className="absolute bottom-0 left-0 w-20 md:w-28 lg:w-32" src="/assets/bottomLeft.png" alt="image"/>
          
--- src/_auth/forms/login.tsx
+++ src/_auth/forms/login.tsx
@@ -81,21 +81,28 @@ const Login: FunctionComponent<LoginProps> = () => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)} className="w-full">
-        <div className="flex flex-col gap-5">
+    <> 
+     <form onSubmit={handleSubmit(onSubmit)} className="py-14 px-6 xs:px-12 lg:px-20 mx-auto bg-white ">
+        <div className="flex flex-col gap-20">
+
+          {/* //logo */}
+          <div className="flex items-center justify-center">
           <img src="/assets/eventy.png" alt="logo" className="w-24" />
+          </div>
+
+          {/* //welcome */}
           <div>
             <h1 className="font-extrabold text-5xl tracking-tighter">
               Login to Your Account
             </h1>
-            <p className="mt-2 font-normal tracking-tight text-base">
+            <p className="mt-2 font-normal tracking-tight text-base pt-2">
               Welcome Back !
             </p>
+            <div className="h-0.5 w-full bg-slate-100"></div>
           </div>
-          <div className="h-0.5 w-full bg-slate-100"></div>
-        </div>
-        <div className="flex flex-col gap-5 mt-5">
+
+          {/* //form fields & accept */}
+          <div className="flex flex-col gap-5 mt-5">
           {(errors.email || errors.password) && (
             <Alert variant="destructive">
               <AlertCircle className="h-4 w-4" />
@@ -118,7 +125,6 @@ const Login: FunctionComponent<LoginProps> = () => {
               placeholder="Email"
             />
           </div>
-
           <div className="grid w-full items-center gap-1.5">
             <Label htmlFor="password">Password</Label>
             <Input
@@ -128,7 +134,6 @@ const Login: FunctionComponent<LoginProps> = () => {
               placeholder="Password"
             />
           </div>
-
           <div>
             <div className="flex items-center space-x-2">
               <Controller
@@ -146,15 +151,24 @@ const Login: FunctionComponent<LoginProps> = () => {
             <p className="text-xs text-slate-400 mt-1 ml-6">
               You agree to our Terms of Service and Privacy Policy.
             </p>
+           </div>
           </div>
-          <Button type="submit" disabled={isSubmitting} className="mt-5">
-            {isSubmitting && <div className="mr -5">Loading...</div>}
-            {!isSubmitting && "Log In"}
-          </Button>
+
+          {/* //button + arrow image relative div*/}
+          <div className="relative">
+          <img src="/assets/button_arrow.png" alt="image" className="absolute top-2 transform -translate-y-1/2 -translate-x-24 w-16 md:w-20 lg:w-24" />  
+        <button  type="submit" disabled={isSubmitting} className="mt-5 w-full h-12 bg-black">
+          <span className="h-12 block translate-x-2 -translate-y-2  border-2 border-black  bg-primary p-2 transition-all hover:-translate-y-3 active:translate-x-0 active:translate-y-0 text-white text-lg text-center">
+          {isSubmitting && <div className="mr -5">Loading...</div>}
+          {!isSubmitting && "Log In"} 
+          </span>
+        </button>
+          </div>
         </div>
       </form>
     </>
   );
 };
+
 
 export default Login;
