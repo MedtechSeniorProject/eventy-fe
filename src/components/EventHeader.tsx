7ecb5f4cf5579a92ea50465faf35b5b599c7545f
Walid Siala <wsiala4@gmail.com>
2024-03-09 12:33:33 +0100

    Added Event Date Formatting

--- src/components/EventHeader.tsx
+++ src/components/EventHeader.tsx
@@ -1,13 +1,16 @@
+import { format } from 'date-fns';
+
 interface props{
     name: string
     time: string
 }
 
 const EventHeader = (props:props) => {
-
+  const dateTime = new Date(props.time);
+  const formattedDateTime = format(dateTime, "EEEE, MMMM d, yyyy 'at' h:mm a");
   return (<>
     <h1 className="font-bold text-3xl">{props.name}</h1>
-    <p>{props.time}</p>
+    <p>{formattedDateTime}</p>
   </>)
 }
 
