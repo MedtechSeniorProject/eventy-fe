0cddfa5ca8d5748988a3a6d116c3cd18a7e8dff1
Walid Siala <wsiala4@gmail.com>
2024-03-09 12:33:17 +0100

    Added AddAttendees Functionality with Excel

--- src/components/UploadAttendee.tsx
+++ src/components/UploadAttendee.tsx
@@ -1,55 +1,63 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { Input } from "./ui/input";
 import { useToast } from "./ui/use-toast";
+import { useAddAttendees } from "@/lib/queries/queries";
+import * as XLSX from 'xlsx';
+import { AddAttendees, AttendeeForm } from "@/types/types";
 
-const UploadAttendee = () => {
+const UploadAttendee = ({...props}) => {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isLoading, setIsLoading] = useState(false);
   const { toast } = useToast();
+  const {mutateAsync: addAttendees } = useAddAttendees()
 
-  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
-    if (file) {
-      const extension = file.name.split(".").pop()?.toLowerCase();
-      if (extension !== "csv") {
-        toast({
-          variant: "destructive",
-          title: "File Format Error",
-          description: "Please select a CSV file",
-        });
+    if (!file) {
+      console.error('No file selected');
+      return;
+    }
 
+    setIsLoading(true);
+
+    const reader = new FileReader();
+    reader.onload = async(e) => {
+      const data = new Uint8Array(e.target?.result as ArrayBuffer);
+      const workbook = XLSX.read(data, { type: 'array' });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 }) as Array<Array<string>>;
+
+      const headers = jsonData[0];
+      const parsedData = jsonData.slice(1).map((row) => {
+        const obj: AttendeeForm = { name: "", email: "" };
+        headers.forEach((header, index) => {
+          obj[header as keyof AttendeeForm] = row[index];
+        });
+        return obj;
+      });
+
+      console.log(parsedData);
+      const params: AddAttendees = {
+        eventId: props.eventId,
+        attendees: parsedData
+      }
+      const response = await addAttendees(params)
+      if(!response.ok){
+        toast({variant:"destructive",title:"Error", description:"Error while uploading attendees to this event!"})
         if (fileInputRef.current) {
-          fileInputRef.current.value = "";
+          fileInputRef.current.value = '';
         }
+        setIsLoading(false);
         return;
       }
-
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const text = e.target?.result as string;
-
-        const rows = text.split("\n");
-        const headers = rows[0].split(",").map((header) => header.trim());
-        const data: any[] = [];
-        for (let i = 1; i < rows.length; i++) {
-          if (rows[i].trim() === "") continue;
-          const values = rows[i].split(",");
-          const rowData: Record<string, string> = {};
-          for (let j = 0; j < headers.length; j++) {
-            rowData[headers[j]] = values[j]?.trim() ?? "";
-          }
-          data.push(rowData);
-        }
-        toast({
-          variant: "default",
-          title: "File Uploaded Successfully",
-          description: `Added ${data.length} Attendees to this event !`,
-        });
-        if (fileInputRef.current) {
-          fileInputRef.current.value = "";
-        }
-      };
-      reader.readAsText(file);
-    }
+      toast({title:"Attendees Added Successfully"})
+      if (fileInputRef.current) {
+        fileInputRef.current.value = '';
+      }
+      setIsLoading(false);
+    };
+    reader.readAsArrayBuffer(file);
   };
 
   return (
@@ -57,10 +65,11 @@ const UploadAttendee = () => {
       <div className="grid w-full max-w-sm items-center gap-1.5">
         <Input
           ref={fileInputRef}
-          accept=".csv"
+          accept=".xlsx"
           id="attendees"
           type="file"
           onChange={handleFileUpload}
+          disabled={isLoading}
         />
       </div>
     </>
