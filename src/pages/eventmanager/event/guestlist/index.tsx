71741def18f14ab1830c7d63546f8aa9bdcede6c
Walid Siala <wsiala4@gmail.com>
2024-03-16 13:58:05 +0100

    Added Layout Shift

--- src/pages/eventmanager/event/guestlist/index.tsx
+++ src/pages/eventmanager/event/guestlist/index.tsx
@@ -20,7 +20,7 @@ const EventGuestList = () => {
   const attendees = event.attendees || [];
 
   return (
-    <div className="p-10 w-full">
+    <div className="p-10 w-11/12">
       <EventHeader name={event.name} time={event.time} />
       <div className="mt-5 flex gap-5">
         <AddAttendee eventId={event.id} />
--- src/pages/eventmanager/events/index.tsx
+++ src/pages/eventmanager/events/index.tsx
@@ -8,7 +8,7 @@ const Events = () => {
 
   return (
     <>
-      <div className="px-10 w-full">
+      <div className="px-10 w-11/12">
         <Header name="Events" />
         <CreateEvent />
         <div className="mt-10">
