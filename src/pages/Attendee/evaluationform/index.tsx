0ce2aa120a7d864bc6c19ba32bab8157684cdcd9
Walid Siala <wsiala4@gmail.com>
2024-04-23 19:16:05 +0100

    Customized Error Handling EvalForm

--- src/pages/Attendee/evaluationform/index.tsx
+++ src/pages/Attendee/evaluationform/index.tsx
@@ -11,6 +11,8 @@ import { useParams } from "react-router-dom";
 import { useToast } from "@/components/ui/use-toast";
 import { responseForm } from "@/types/types";
 import SEO from "@/components/SEO";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircle } from "lucide-react";
 
 const AttendeeEvaluationForm = () => {
   const { eventId, inviteeId } = useParams() as {
@@ -34,8 +36,12 @@ const AttendeeEvaluationForm = () => {
 
   if (isLoading) {
     return (
-      <div className="flex w-full h-screen justify-center items-center font-semibold">
-        Loading...
+      <div className="flex flex-col w-full h-screen justify-center items-center font-semibold">
+        <div className="flex flex-col justify-center items-center">
+          <img src="/assets/eventy.png" width={50} />
+          <h1 className="my-5 font-bold text-3xl underline">Evaluation Form</h1>
+        </div>
+          <div>Loading...</div>
       </div>
     );
   }
@@ -43,11 +49,15 @@ const AttendeeEvaluationForm = () => {
   if (isError || event?.length === 0) {
     return (
       <div className="flex flex-col w-full h-screen justify-center items-center font-semibold">
-        <div className="mb-3 text-2xl underline text-red-500">
-          {(error?.response?.data as any)?.name ||
-            "Error"}
-        </div>
-        <div>{(error?.response?.data as any)?.message || "Failed to load event"}</div>
+        <img src="/assets/eventy.png" width={50} />
+        <h1 className="my-5 font-bold text-3xl underline">Evaluation Form</h1>
+        <Alert variant="destructive" className="w-1/2">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>
+            {(error?.response?.data as any)?.message || "Failed to load event"}
+          </AlertDescription>
+        </Alert>
       </div>
     );
   }
@@ -140,72 +150,37 @@ const AttendeeEvaluationForm = () => {
         title="Eventy - Evaluation Form"
         description="Event Management System Evaluation Form Page"
         name="Eventy"
-        type="evaluation form" />
-    
-    <div className="w-full h-full">
-    <div className="flex flex-col items-center py-10">
-      <img src="/assets/eventy.png" width={50} />
-      <h1 className="mt-5 font-bold text-3xl underline">Evaluation Form</h1>
-      <div className="my-5 border-2 border-black p-5 w-11/12 md:w-6/12">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5 mt-3">
-          {event?.map((question, index) => {
-            return (
-              <div key={index}>
-                <div className="flex justify-between">
-                  <h3 className="font-medium mb-2">
-                    Question {index + 1}: {question.question}{" "}
-                    <span className="text-red-500 font-semibold">
-                      {question.isRequired && "**"}
-                    </span>
-                  </h3>
-                </div>
-                {question.type === "Checkbox" && (
-                  <>
-                    {Array.isArray(question.options) &&
-                      question.options.map((option, idx) => {
-                        return (
-                          <div key={idx}>
-                            <input
-                              type="checkbox"
-                              className="regular-checkbox"
-                              value={option}
-                              name={question.id}
-                              onChange={(e: any) =>
-                                handleInputChange(e, question.id, question.type)
-                              }
-                            />
-                            <Label className="ml-2">{option}</Label>
-                          </div>
-                        );
-                      })}
-                  </>
-                )}
-                {question.type === "Input" && (
-                  <Input
-                    required={question.isRequired}
-                    className="w-full xl:w-1/2"
-                    type="text"
-                    onChange={(e: any) =>
-                      handleInputChange(e, question.id, question.type)
-                    }
-                    name={question.question}
-                  />
-                )}
-                {question.type === "Radio" && (
-                  <>
-                    <RadioGroup>
-                      {Array.isArray(question.options) &&
-                        question.options.map((option, idx) => {
-                          return (
-                            <div key={idx}>
-                              <div className="flex items-center space-x-2">
+        type="evaluation form"
+      />
+
+      <div className="w-full h-full">
+        <div className="flex flex-col items-center py-10">
+          <img src="/assets/eventy.png" width={50} />
+          <h1 className="mt-5 font-bold text-3xl underline">Evaluation Form</h1>
+          <div className="my-5 border-2 border-black p-5 w-11/12 md:w-6/12">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-5 mt-3">
+              {event?.map((question, index) => {
+                return (
+                  <div key={index}>
+                    <div className="flex justify-between">
+                      <h3 className="font-medium mb-2">
+                        Question {index + 1}: {question.question}{" "}
+                        <span className="text-red-500 font-semibold">
+                          {question.isRequired && "**"}
+                        </span>
+                      </h3>
+                    </div>
+                    {question.type === "Checkbox" && (
+                      <>
+                        {Array.isArray(question.options) &&
+                          question.options.map((option, idx) => {
+                            return (
+                              <div key={idx}>
                                 <input
-                                  type="radio"
-                                  required={question.isRequired}
+                                  type="checkbox"
+                                  className="regular-checkbox"
                                   value={option}
-                                  className="regular-radio"
                                   name={question.id}
-                                  id={option}
                                   onChange={(e: any) =>
                                     handleInputChange(
                                       e,
@@ -214,26 +189,66 @@ const AttendeeEvaluationForm = () => {
                                     )
                                   }
                                 />
-                                <Label htmlFor={option}>{option}</Label>
+                                <Label className="ml-2">{option}</Label>
                               </div>
-                            </div>
-                          );
-                        })}
-                    </RadioGroup>
-                  </>
-                )}
+                            );
+                          })}
+                      </>
+                    )}
+                    {question.type === "Input" && (
+                      <Input
+                        required={question.isRequired}
+                        className="w-full xl:w-1/2"
+                        type="text"
+                        onChange={(e: any) =>
+                          handleInputChange(e, question.id, question.type)
+                        }
+                        name={question.question}
+                      />
+                    )}
+                    {question.type === "Radio" && (
+                      <>
+                        <RadioGroup>
+                          {Array.isArray(question.options) &&
+                            question.options.map((option, idx) => {
+                              return (
+                                <div key={idx}>
+                                  <div className="flex items-center space-x-2">
+                                    <input
+                                      type="radio"
+                                      required={question.isRequired}
+                                      value={option}
+                                      className="regular-radio"
+                                      name={question.id}
+                                      id={option}
+                                      onChange={(e: any) =>
+                                        handleInputChange(
+                                          e,
+                                          question.id,
+                                          question.type
+                                        )
+                                      }
+                                    />
+                                    <Label htmlFor={option}>{option}</Label>
+                                  </div>
+                                </div>
+                              );
+                            })}
+                        </RadioGroup>
+                      </>
+                    )}
+                  </div>
+                );
+              })}
+              <div className="flex justify-center">
+                <Button variant="inverse" type="submit" disabled={loading}>
+                  {loading ? "Loading..." : "Submit Form"}
+                </Button>
               </div>
-            );
-          })}
-          <div className="flex justify-center">
-            <Button variant="inverse" type="submit" disabled={loading}>
-              {loading ? "Loading..." : "Submit Form"}
-            </Button>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
-    </div>
-    </div>
     </>
   );
 };
