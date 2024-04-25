73c4523441ec0339962faf55175578dece460408
Walid Siala <wsiala4@gmail.com>
2024-04-25 14:10:35 +0100

    Added Settings UI

--- src/pages/eventmanager/settings/index.tsx
+++ src/pages/eventmanager/settings/index.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/Header"
 import SEO from "@/components/SEO"
 
 const EmSettings = () => {
@@ -8,7 +9,14 @@ const EmSettings = () => {
         description="Event Management System Settings Page"
         name="Eventy"
         type="settings" />
-        EM Settings</div>
+        <div>
+          <Header name="Settings"/>  
+          <div className="mt-5 flex flex-col gap-1">
+            <div className="underline font-medium hover:text-primary hover:cursor-pointer w-fit">Change Username</div>
+            <div className="underline font-medium hover:text-primary hover:cursor-pointer w-fit">Change Password</div>
+          </div>
+        </div>  
+      </div>
     )
   }
   
