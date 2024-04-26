c4d3910e5e7a06c95df52fa9a0776c54ec5c680d
Walid Siala <wsiala4@gmail.com>
2024-04-26 09:40:42 +0100

    Added Alert confirmation

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -15,26 +15,26 @@ import {
     name: string,
     disabled?: boolean
     cta: () => void
+    className?: string
   }
 
-  export function AlertConfirmation({name, disabled, cta} : AlertConfirmationProps) {
+  export function AlertConfirmation({name, disabled, cta, className} : AlertConfirmationProps) {
     return (
       <AlertDialog>
         <AlertDialogTrigger asChild>
-          <Button disabled={disabled} variant="outline">{name}</Button>
+          <Button disabled={disabled} className={className}>{name}</Button>
         </AlertDialogTrigger>
         <AlertDialogContent>
           <AlertDialogHeader>
             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
             <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              This action cannot be undone. This will permanently remove your data from our servers.
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
             <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction onClick={() => cta()}>
-                    Continue
+            <AlertDialogAction asChild onClick={() => cta()}>
+                  <Button className=" text-white bg-black hover:bg-white hover:text-black hover:border hover:border-black ">Continue</Button>
             </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
